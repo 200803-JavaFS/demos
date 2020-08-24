5ac40ba9a4aaedf81a0124e5b3b9d519b2f2829a
gattietime <gattietime@gmail.com>
2020-08-24 12:00:46 -0400

    authent

--- Week3/HelloJackson/src/main/java/com/revature/web/MasterServlet.java
+++ Week3/HelloJackson/src/main/java/com/revature/web/MasterServlet.java
@@ -17,7 +17,7 @@ public class MasterServlet extends HttpServlet {
 
 	private static AvengerController ac = new AvengerController();
 	private static LoginController lc = new LoginController();
-	
+
 	public MasterServlet() {
 		super();
 	}
@@ -33,25 +33,29 @@ public class MasterServlet extends HttpServlet {
 
 		final String URI = req.getRequestURI().replace("/HelloJackson/", "");
 
-		// example URI = avenger/1 to get avenger with ID 1 
-		
+		// example URI = avenger/1 to get avenger with ID 1
+
 		String[] portions = URI.split("/");
 
 		System.out.println(Arrays.toString(portions));
 
-		
 		try {
 			switch (portions[0]) {
 			case "avenger":
-				if (req.getMethod().equals("GET")) {
-					if (portions.length == 2) {
-						int id = Integer.parseInt(portions[1]);
-						ac.getAvenger(res, id);
-					}else if (portions.length == 1){
-						ac.getAllAvengers(res);
+				if (req.getSession(false) != null && (boolean) req.getSession().getAttribute("loggedin")) {
+					if (req.getMethod().equals("GET")) {
+						if (portions.length == 2) {
+							int id = Integer.parseInt(portions[1]);
+							ac.getAvenger(res, id);
+						} else if (portions.length == 1) {
+							ac.getAllAvengers(res);
+						}
+					} else if (req.getMethod().equals("POST")) {
+						ac.addAvenger(req, res);
 					}
-				} else if(req.getMethod().equals("POST")) {
-					ac.addAvenger(req, res);
+				} else {
+					res.setStatus(403);
+					res.getWriter().println("You must be logged in to do that!");
 				}
 				break;
 			case "login":
@@ -61,8 +65,7 @@ public class MasterServlet extends HttpServlet {
 				lc.logout(req, res);
 				break;
 			}
-			
-				
+
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 			res.getWriter().print("The id you provided is not an integer");
